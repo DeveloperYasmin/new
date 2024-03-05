d0c62bf6d3905ee682e74e51702590604d7b4852
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-05 15:12:19 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,18 @@ import Link from "next/link"
 const page = () => {
  
   return (
+    <div>
+      <ul>
+        <li>
+        <Link href="/">Home</Link>
+          </li>
+          <li>
+        <Link href="/about">About</Link>
+          </li> <li>
+        <Link href="/contact">Contact</Link>
+          </li>
+      </ul>
+  
     <div className="flex gap-5 mt-5">
       <div className="flex-1 flex flex-col gap-12 ">
       <h1 className="md:text-6xl text-5xl p-[3px] bg-gradient-to-r from-yellow-600 to-white text-transparent bg-clip-text">Creative Thoughts Agency</h1>
@@ -17,6 +29,7 @@ const page = () => {
       </div>
       </div> 
       <div className="relative w-56  h-96 flex-1"><Image src="/hero.gif" alt="hero" fill/>
+      </div>
       </div>
       </div>
   )
