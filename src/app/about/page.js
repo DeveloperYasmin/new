56b219fe37a5e2a58aeeb23028b6fd597f14678f
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-05 13:32:55 +0530

    main

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,52 +1,34 @@
-import Image from "next/image";
-
+import Image from "next/image"
 export const metadata = {
   title: "About Page",
-  description: "About description",
+  description: "About Description",
 };
-
-
-const AboutPage = () => {
-
-  // console.log("lets check where it works")
+const Aboutpage = () => {
   return (
-    <div className= {""}>
-      <div >
-        <h2 >About Agency</h2>
-        <h1 >
-          We create digital ideas that are bigger, bolder, braver and better.
-        </h1>
-        <p >
-          We create digital ideas that are bigger, bolder, braver and better. We
-          believe in good ideas flexibility and precission We’re world’s Our
-          Special Team best consulting & finance solution provider. Wide range
-          of web and software development services.
-        </p>
-        <div >
-          <div >
-            <h1>10 K+</h1>
-            <p>Year of experience</p>
-          </div>
-          <div >
-            <h1>10 K+</h1>
-            <p>Year of experience</p>
-          </div>
-          <div >
-            <h1>10 K+</h1>
-            <p>Year of experience</p>
-          </div>
-        </div>
+    <div className="flex gap-28">
+      <div className="flex-1 flex flex-col gap-12">
+        <h1 className="text-yellow-500 text-2xl">About Agency </h1>
+        <h1 className="md:text-5xl text-3xl ">We create digital ideas that are bigger,bolder,braver and better</h1>
+        <p className="text-xl  font-light text-stone-400">We create digital ideas that are bigger,bolder,braver and better.We believe in good ideas flexibility and precission. Our special team best consulting & finance solution provider.Wide range of web and software development services.</p>
+    <div className=" md:flex items-center justify-between">
+      <div className="flex flex-col gap-2 ">
+      <h1 className="text-4xl text-yellow-500 font-sans font-bold">10K+</h1>
+      <p>Year of Experience</p>
+    </div>
+    <div className="flex flex-col gap-2 md:mt-0 mt-4">
+      <h1 className="text-4xl text-yellow-500 font-sans font-bold">234K+</h1>
+      <p>People Reached</p>
+    </div>      <div className="flex flex-col gap-2 md:mt-0 mt-4">
+      <h1 className="text-4xl text-yellow-500 font-sans font-bold">9K+</h1>
+      <p>Services and plugins</p>
+    </div>
       </div>
-      <div >
-        <Image
-          src="/about.png"
-          alt="About Image"
-          fill
-          
-        />
+      </div>
+      <div className="flex-1  relative">
+       <Image className="object-contain" src="/horse.png" alt="pic" fill/>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AboutPage;
+export default Aboutpage
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,25 @@
-import Image from "next/image";
+import Image from "next/image"
+import Link from "next/link"
 
-const Home = () => {
+const page = () => {
+ 
   return (
-    <div className={""}>
-      <div >
-        <h1 >Creative Thoughts Agency.</h1>
-        <p >
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
-          blanditiis adipisci minima reiciendis a autem assumenda dolore.
-        </p>
-        <div >
-          <button >Learn More</button>
-          <button >Contact</button>
-        </div>
-        <div >
-          <Image src="/brands.png" alt="" fill />
-        </div>
+    <div className="flex gap-5 mt-5">
+      <div className="flex-1 flex flex-col gap-12 ">
+      <h1 className="md:text-6xl text-5xl p-[3px] bg-gradient-to-r from-yellow-600 to-white text-transparent bg-clip-text">Creative Thoughts Agency</h1>
+      <p className="md:text-lg text-md">Creativity is seeing what everyone else has seen, and thinking what no one else has thought</p>
+      <div  className="flex md:gap-5 gap-2">
+        <Link legacyBehavior href="/about"><a className="p-5 min-w-32 cursor-pointer border-none border-spacing-1 text-black font-semibold rounded-md bg-yellow-600">Learn More</a></Link>
+        <Link href="/contact"><button className="p-5 text-black font-semibold min-w-32 cursor-pointer border-none border-spacing-1 rounded-md bg-white ">Contact</button></Link>
       </div>
-      
-    </div>
-  );
-};
+      <div className=" md:w-[500px] md:w[200px] h-[50px] relative grayscale">
+        <Image src="/brands.png" alt="brands" fill/>
+      </div>
+      </div> 
+      <div className="relative w-56  h-96 flex-1"><Image src="/hero.gif" alt="hero" fill/>
+      </div>
+      </div>
+  )
+}
 
-export default Home;
+export default page
