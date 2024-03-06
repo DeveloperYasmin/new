b2e76a9df76854e2d7bc62c318b5bd89754eabf6
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-06 12:48:42 +0530

    main

--- src/app/lib/auth.config.js
+++ src/app/lib/auth.config.js
@@ -23,7 +23,17 @@ export const authConfig={
             const isOnAdminPanel=request.nextUrl?.pathname.startsWith("/admin")
             const isOnBlogPage=request.nextUrl?.pathname.startsWith("/blog")
             const isOnLoginPage=request.nextUrl?.pathname.startsWith("/login")
-            
+            //ONLY ADMIN CAN REACH THE ADMIN DASHBOARD
+             if(isOnAdminPanel && !user?.isAdmin)
+             {return false}
+            //ONLY AUTHENTICATED USERS CAN REACH THE BLOG PAGE
+
+            if(isOnBlogPage && !user)
+            {return false}
+            //ONLY AUTHENTICATED USERS CAN REACH THE LOGIN PAGE
+            if(isOnLoginPage && user)
+            {return Response.redirect(new URL("/",request.nextUrl))}
+            return true
         }
         
     }
