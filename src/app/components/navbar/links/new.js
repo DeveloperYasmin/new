f10a01c5bb8d6903cf87c52e9e921555ac40f53f
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-07 19:26:54 +0530

    main

--- src/app/components/navbar/links/new.js
+++ src/app/components/navbar/links/new.js
@@ -1,23 +1,12 @@
 "use client"
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import Navlink from '../navlink/navlink'
 import Image from 'next/image'
 import { handleLogout } from '@/app/lib/action'
 
 
     const links = [
-        {
-            title:"Home",
-            path:"/"
-        },
-        {
-            title:"About",
-            path:"/about"
-        },
-        {
-            title:"Contact",
-            path:"/contact"
-        },
+       
         {
             title:"Blog",
             path:"/blog"
@@ -27,13 +16,7 @@ import { handleLogout } from '@/app/lib/action'
 
     const news =({session})=>{
         const [open,setopen]=useState(false)
-    const Menuref=useRef()
-    const Imgref=useRef()
-    window.addEventListener("click",(e)=>{
-        if(e.target !== Menuref.current && e.target !== Imgref.current){
-            setopen(false) 
-        }
-    })
+    
     //TEMPORARY
     const isAdmin=true
   return (
@@ -53,9 +36,9 @@ import { handleLogout } from '@/app/lib/action'
         <Navlink item={{title:"Login",path:"/login"}}/>
         )}
     </div>
-    <Image ref={Imgref} className='lg:hidden cursor-pointer' src="/menu.png" alt=" " width={30} height={30} onClick={()=>setopen(!open)}></Image>
+    <Image  className='lg:hidden cursor-pointer' src="/menu.png" alt=" " width={30} height={30} onClick={()=>setopen(!open)}></Image>
     {
-        open && (<div ref={Menuref} onClick={()=>setopen(false)} className='absolute z-20 h-[85%] rounded-xl top-24 lg:hidden right-0 w-[50%] flex flex-col items-center justify-center gap-3  bg-stone-900'>
+        open && (<div  onClick={()=>setopen(false)} className='absolute z-20 h-[85%] rounded-xl top-24 lg:hidden right-0 w-[50%] flex flex-col items-center justify-center gap-3  bg-stone-900'>
     {links.map((link=>(
             <Navlink  item={link} key={link.title}/>
         )))}{session?.user?(
