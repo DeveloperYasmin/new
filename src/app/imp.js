4e9526b1390edf2616e81ac8666c7994181c7035
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-07 16:14:44 +0530

    main

--- src/app/imp.js
+++ src/app/imp.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
+import About from "./about"
 
 const Imp = ()=> {
   
@@ -12,7 +13,7 @@ const Imp = ()=> {
     <div className="lg:flex "> 
       <ul className='lg:flex items-center gap-8 hidden'>
         <li ><Link className="  hover:bg-yellow-500  hover:text-black  min-w-24 p-2 rounded-full font-medium text-center" href="/">Home</Link></li>
-          <li><Link className="  hover:bg-yellow-500 hover:text-black  min-w-24 p-2 rounded-full font-medium text-center"  href="./about">About</Link></li> 
+          <li><Link className="  hover:bg-yellow-500 hover:text-black  min-w-24 p-2 rounded-full font-medium text-center"  href={`${About}`}>About page</Link></li> 
           <li><Link className="  hover:bg-yellow-500 hover:text-black  min-w-24 p-2 rounded-full font-medium text-center" href="/contact">Contact</Link></li>
           <li><Link className="  hover:bg-yellow-500 hover:text-black  min-w-24 p-2 rounded-full font-medium text-center" href="/blog">Blog</Link></li> 
 
