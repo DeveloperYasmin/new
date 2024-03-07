662c79c844583c30dae380deaffc693831eeefd5
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-07 15:08:21 +0530

    upsted

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Image from "next/image";
 
@@ -19,7 +18,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} bg-black text-white m-0 p-0 border-spacing-0`}>
         <div className="select-none font-serif min-h-dvh m-auto pl-[50px] pr-[50px] flex flex-col justify-between ">
-        <Navbar/>
         {children}
         <Footer/>
         </div>
