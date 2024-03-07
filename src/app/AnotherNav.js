9fb55e04e343ac9eb05afed1a8b7eb7e8967c24e
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-07 20:42:20 +0530

    new

--- src/app/AnotherNav.js
+++ src/app/AnotherNav.js
@@ -24,7 +24,7 @@ const Navbar = ({session})=> {
       
     <div className="lg:flex "> 
       <ul className='lg:flex items-center gap-8 hidden'>
-       <li className="mr-[650px]"> <Link href="/"><Image className="font-bold text-3xl flex " src="/logo.png" width={130} height={160}/></Link> </li>
+       <li className="mr-[650px]"> <Link href="/"><Image className="font-bold text-3xl flex " alt="" src="/logo.png" width={130} height={160}/></Link> </li>
         <li ><Link className="  hover:bg-yellow-500  hover:text-black  min-w-24 p-2 rounded-full font-medium text-center" href="/">Home</Link></li>
           <li><Link className="  hover:bg-yellow-500 hover:text-black  min-w-24 p-2 rounded-full font-medium text-center"  href="/about">About</Link></li> 
           <li><Link className="  hover:bg-yellow-500 hover:text-black  min-w-24 p-2 rounded-full font-medium text-center" href="/contact">Contact</Link></li>
@@ -41,7 +41,7 @@ const Navbar = ({session})=> {
              </>
 
         ):(
-        <Navlink item={{title:"Login",path:"/login"}}/>
+          <Link className="  hover:bg-yellow-500 hover:text-black  min-w-24 p-2 rounded-full font-medium text-center" href="/login">login</Link>
         )}
          </div>
            <Image ref={Imgref} className='lg:hidden cursor-pointer' src="/menu.png" alt=" " width={30} height={30} onClick={()=>setopen(!open)}></Image>
