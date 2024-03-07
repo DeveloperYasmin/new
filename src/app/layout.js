d4926c23e832b0f8378a65984be6c9407ddc6ba2
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-07 20:51:29 +0530

    new

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer/Footer";
 import AnotherNav from "./AnotherNav";
-import AnotherNavlink from "./AnotherNavlink";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} bg-black text-white m-0 p-0 border-spacing-0`}>
         <div className="select-none font-serif min-h-dvh m-auto pl-[50px] pr-[50px] flex flex-col justify-between ">
         <AnotherNav/>
-        <AnotherNavlink/>
         {children}
         <Footer/>
         </div>
